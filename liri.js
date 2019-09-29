e2d9f047fd12833124953ac63b59fc237d96c959
Ckopei <cameron@kopels.com>
2019-09-29 18:46:25 -0400

    Spotify function is working as intended.

--- liri.js
+++ liri.js
@@ -7,19 +7,23 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var commands = process.argv[2];
-var userInput = process.argv[3];
+//take the process.argv and removes the first 3 items in the array, being node, lirijs, and command. Joins them all with a space.
+var userInput = process.argv.slice(3).join(" ");
 
 //four function. 
 function spotifyThis(input) {
     spotify
-  .search({ type: 'track', query: input })
+  .search({ type: 'track', query: input, limit: 1 })
   .then(function(response) {
-    console.log(response);
+    // console.log(JSON.stringify(response, null, 2));
+    console.log("Artist: " +response.tracks.items[0].artists[0].name)
+    console.log("Song: " +response.tracks.items[0].name)
+    console.log("Preview link: "+response.tracks.items[0].external_urls.spotify)
+    console.log("Album: " +response.tracks.items[0].album.name)
   })
   .catch(function(err) {
     console.log(err);
   });
-
 }
 
 function concertThis() {
